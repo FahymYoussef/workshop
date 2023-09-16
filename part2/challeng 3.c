fc786fd13c7a3307685807125ac0db111dcd4975
yousseffahym <youssef.fahym112@gmail.com>
2023-09-16 15:37:51 +0100

    from 1 to 3

---
+++ part2/challeng 3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    int c;
+    printf("enter your somme:");
+    scanf("%d %d",&a,&b);
+    c= a*a*a;
+    if(a==b){
+    printf("your somme %d",c);
+
+    }
+    return 0;
+}
