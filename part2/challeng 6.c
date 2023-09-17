5ff0eb1b173829153a9831afae7eaaf0272c3822
yousseffahym <youssef.fahym112@gmail.com>
2023-09-17 16:10:38 +0100

    challenge 6

---
+++ part2/challeng 6.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+main()
+{
+
+    int number;
+    printf("enter your number:");
+    scanf("%d",&number);
+    if (number>0)
+    {
+        printf("your number is positive");
+    }
+    else if (number<0)
+    {
+        printf("your number is negative");
+    }
+    else {
+        printf("the number is zero");
+    }
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
